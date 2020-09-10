4ead3f524cd7ec9d9e49a7d55f37455a16b98637
Edward Forgacs <eddie.forgacs@gmail.com>
2020-09-10 19:26:09 -0400

    Split tutorial and misty files

---
+++ tutorial.py
@@ -0,0 +1,33 @@
+# print("Hello world! This is Misty")
+a = 1
+b = 2
+# print(a + b)
+# print("Eddie is the man, lets go to Korea")
+food = ["ramen", "soda", "milk", "ramen"]
+# print(f"{food[2]} is delicious")
+# for food_item in food:
+#     if food_item == "ramen":
+#         print("spicy")
+#     else:
+#         print("not spicy")
+
+drinks = ["water"]
+for drink in drinks:
+    print(drink)
+# n=11
+# while n>10:
+#     print(f"Are You Ready For Some Football {n}")
+#     n=n+15
+
+# fruit_and_vegetables = {
+#     "apple": "fruit",
+#     "banana": "fruit",
+#     "orange": "fruit",
+#     "cucumber": "vegetable",
+#     "spinach": "vegetable",
+# }
+# # print(fruit_and_vegetables["apple"])
+# # print(fruit_and_vegetables.values())
+# for green, green_type in fruit_and_vegetables.items():
+#     # print(green, green_type )
+#     print(f"{green} is a {green_type}")
